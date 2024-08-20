3c59271782cc39a3e16c3d327181b35c82a357c0
zhaohehuhu <luoyedeyi@163.com>
2024-08-20 17:45:01 +0800

    refactor

--- worker/src/main/scala/org/apache/celeborn/service/deploy/worker/storage/FlushTask.scala
+++ worker/src/main/scala/org/apache/celeborn/service/deploy/worker/storage/FlushTask.scala
@@ -17,13 +17,13 @@
 
 package org.apache.celeborn.service.deploy.worker.storage
 
+import java.io.{ByteArrayInputStream, InputStream}
 import java.nio.channels.FileChannel
-
+import java.util
 import io.netty.buffer.{ByteBufUtil, CompositeByteBuf}
 import org.apache.hadoop.fs.Path
-import org.apache.hadoop.io.IOUtils
-
 import org.apache.celeborn.common.protocol.StorageInfo.Type
+import org.apache.celeborn.reflect.{DynClasses, DynMethods}
 
 abstract private[worker] class FlushTask(
     val buffer: CompositeByteBuf,
@@ -58,33 +58,96 @@ private[worker] class HdfsFlushTask(
     hdfsStream.write(ByteBufUtil.getBytes(buffer))
     hdfsStream.close()
   }
+
+
 }
 
 private[worker] class S3FlushTask(
     buffer: CompositeByteBuf,
     val path: Path,
     notifier: FlushNotifier,
-    keepBuffer: Boolean) extends FlushTask(buffer, notifier, keepBuffer) {
+    keepBuffer: Boolean, s3Flusher: S3Flusher) extends FlushTask(buffer, notifier, keepBuffer) {
+
+  val hadoopFs = s3Flusher.hadoopFs
+
+  val bucketName = s3Flusher.bucketName
+
+  var s3Client = s3Flusher.s3Client
+
+  var uploadId = s3Flusher.uploadId
+
+  var partNumber = s3Flusher.partNumber
+
+  val partETags = s3Flusher.partETags
+
   override def flush(): Unit = {
-    val hadoopFs = StorageManager.hadoopFs.get(Type.S3)
-    if (hadoopFs.exists(path)) {
-      val conf = hadoopFs.getConf
-      val tempPath = new Path(path.getParent, path.getName + ".tmp")
-      val outputStream = hadoopFs.create(tempPath, true, 256 * 1024)
-      val inputStream = hadoopFs.open(path)
-      try {
-        IOUtils.copyBytes(inputStream, outputStream, conf, false)
-      } finally {
-        inputStream.close()
-      }
-      outputStream.write(ByteBufUtil.getBytes(buffer))
-      outputStream.close()
-      hadoopFs.delete(path, false)
-      hadoopFs.rename(tempPath, path)
-    } else {
-      val s3Stream = hadoopFs.create(path, true, 256 * 1024)
-      s3Stream.write(ByteBufUtil.getBytes(buffer))
-      s3Stream.close()
+    val bytes = ByteBufUtil.getBytes(buffer)
+    var bytesRead = 0
+    val partSize = 50 * 1024 * 1024
+
+    while (bytesRead < bytes.length) {
+      val currentPartSize = Math.min(partSize, bytes.length - bytesRead)
+      val inputStream = new ByteArrayInputStream(bytes, bytesRead, currentPartSize)
+      val uploadRequest = DynClasses
+        .builder()
+        .impl("com.amazonaws.services.s3.model.UploadPartRequest")
+        .build()
+        .getDeclaredConstructor()
+        .newInstance()
+
+      val uploadRequestWithBucket = DynMethods
+        .builder("withBucketName")
+        .impl(uploadRequest.getClass, classOf[String])
+        .build()
+        .invoke(uploadRequest, bucketName)
+
+      val uploadRequestWithKey = DynMethods
+        .builder("withKey")
+        .impl(uploadRequestWithBucket.getClass, classOf[String])
+        .build()
+        .invoke(uploadRequestWithBucket, path.toString)
+
+      val uploadRequestWithUploadId = DynMethods
+        .builder("withUploadId")
+        .impl(uploadRequestWithKey.getClass, classOf[String])
+        .build()
+        .invoke(uploadRequestWithKey, uploadId)
+
+      val uploadRequestWithPartNumber = DynMethods
+        .builder("withPartNumber")
+        .impl(uploadRequestWithUploadId.getClass, classOf[Int])
+        .build()
+        .invoke(uploadRequestWithUploadId, partNumber)
+
+      val uploadRequestWithInputStream = DynMethods
+        .builder("withInputStream")
+        .impl(uploadRequestWithPartNumber.getClass, classOf[InputStream])
+        .build()
+        .invoke(uploadRequestWithPartNumber, inputStream)
+
+      val uploadRequestWithPartSize = DynMethods
+        .builder("withPartSize")
+        .impl(uploadRequestWithInputStream.getClass, classOf[Long])
+        .build()
+        .invoke(uploadRequestWithInputStream, currentPartSize)
+
+      val uploadResult = DynMethods
+        .builder("uploadPart")
+        .impl(s3Client.getClass, uploadRequestWithPartSize.getClass)
+        .build()
+        .invoke(s3Client, uploadRequestWithPartSize)
+
+      val partETag = DynMethods.builder("getPartETag")
+        .impl(uploadResult.getClass)
+        .build()
+        .invoke(uploadResult)
+
+      partETags.add(partETag)
+
+      bytesRead += currentPartSize
+      partNumber += 1
     }
   }
+
+
 }
